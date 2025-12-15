f3aa19ca955d91373c21171b7833405bcf3d0dbd
Dalton Payne <dalton.payne@dvn.com>
2025-12-15 08:38:27 -0600

    add grug-far

--- nvim/lua/lazy-plugins.lua
+++ nvim/lua/lazy-plugins.lua
@@ -66,6 +66,8 @@ require('lazy').setup({
   require 'custom.plugins.claudecode',
 
   require 'custom.plugins.dadbod',
+
+  require 'custom.plugins.grug-far',
   -- kThe following comments only work if you have downloaded the kickstart repo, not just copy pasted the
   --
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
